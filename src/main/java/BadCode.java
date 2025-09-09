76fa5c3ec486082ac5dc87b9bec1b3b06712b42e
Ari Adamo <anarianna657@gmail.com>
2025-09-09 13:13:00 -0400

    correct

--- src/main/java/BadCode.java
+++ src/main/java/BadCode.java
@@ -1,5 +1,6 @@
-public class hi {	
+package main.java;
+public class BadCode {	
 	public static void main(String[] args) {
-		System.out.println("Hello world")
+		System.out.println("Hello world");
 	}
 }
