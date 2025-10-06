29c2456d0cecd9b70161b5acb1788bb29e35db4a
Ari Adamo <anarianna657@gmail.com>
2025-10-06 12:00:56 -0400

    StorageAPI
    Read input and write output

---
+++ Checkpoint2/src/api2/StorageAPI.java
@@ -0,0 +1,9 @@
+package api2;
+
+import project.annotations.ProcessAPI;
+
+@ProcessAPI
+public interface StorageAPI {
+    void readInput(String source);
+    void writeOutput(String destination);
+}
---
+++ Checkpoint2/src/api2/StoragePrototype.java
@@ -0,0 +1,8 @@
+package api2;
+
+public class StoragePrototype {
+    public void prototype(StorageAPI storageapi) {
+        storageapi.readInput("Hello world");
+        storageapi.writeOutput(null);
+    }
+}
