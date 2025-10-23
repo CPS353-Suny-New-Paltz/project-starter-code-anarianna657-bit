3063c177223e6aa8730a9624672007c15bd45070
Ari Adamo <anarianna657@gmail.com>
2025-10-23 11:00:56 -0400

    annotated prototypes

--- src/project/annotations/api/UserPrototype.java
+++ src/project/annotations/api/UserPrototype.java
@@ -1,6 +1,8 @@
 package project.annotations.api;
+import project.annotations.NetworkAPIPrototype;
 
 public class UserPrototype {
+	@NetworkAPIPrototype
     public void prototype(UserAPI userapi) {
     	 userapi.setInput("file://inputs.txt");
          userapi.setOutput("file://outputs.txt");
