be48baeffbc56756bf674d63a9c95230719757e6
Ari Adamo <anarianna657@gmail.com>
2025-10-29 09:33:28 -0400

    skeletonSpooky1 (#39)
    * skeletonSpooky1
    
    * added to main, will edit later
    
    * debugged

---
+++ src/project/EngineAPIImpl.java
@@ -0,0 +1,13 @@
+package project;
+import project.annotations.EngineAPI; 
+
+public abstract class EngineAPIImpl implements EngineAPI {
+
+    public EngineAPIImpl() {
+        // no setup yet
+    }
+
+    public String calculatePrimes(int input) {
+        return "";
+    }
+}
