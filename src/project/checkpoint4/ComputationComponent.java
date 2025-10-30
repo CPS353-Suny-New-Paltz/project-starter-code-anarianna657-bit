922ab5149039e667186447ef2323149e208065ab
Ari Adamo <anarianna657@gmail.com>
2025-10-30 11:09:06 -0400

    Create ComputationComponent.java (#50)
    * Create ComputationComponent.java
    
    Step one of checkpoint 4 works with conceptual and follow declared computation
    
    * Update package name
    
    moving checkpoint4 into projects to attempt to pass the checkstyle
    
    * Update ComputationComponent.java
    
    fixing curly brackets to pass checkstyle

---
+++ src/project/checkpoint4/ComputationComponent.java
@@ -0,0 +1,45 @@
+package project.checkpoint4;
+import java.util.ArrayList;
+import java.util.List;
+import project.annotations.ConceptualAPI;
+
+@ConceptualAPI
+public abstract class ComputationComponent {
+
+    public List<Integer> compute(int limit) {
+        List<Integer> primes = new ArrayList<>();
+
+        if (limit <= 2) {
+            return primes; 
+        }
+
+        for (int i = 2; i < limit; i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+
+    private boolean isPrime(int n) {
+    	
+        if (n < 2) {
+        	return false;
+        }
+        
+        if (n == 2) {
+        	return true;
+        }
+        
+        if (n % 2 == 0) {
+        	return false;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
