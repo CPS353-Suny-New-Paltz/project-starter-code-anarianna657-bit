84e4c8e69896d5b46547a7b618516a9cc53f49d2
Ari Adamo <anarianna657@gmail.com>
2025-10-30 16:18:18 -0400

    Update UserAPIImpl.java

--- src/project/UserAPIImpl.java
+++ src/project/UserAPIImpl.java
@@ -7,52 +7,35 @@ import project.annotations.UserAPI;
 
 public class UserAPIImpl implements UserAPI {
 
-    private EngineAPI engineApi;
-    private StorageAPI storageApi;
+	  private final EngineAPI engine;
+	  private final StorageAPI storage;
 
-    public UserAPIImpl(EngineAPI engineApi, StorageAPI storageApi) {
-        this.engineApi = engineApi;
-        this.storageApi = storageApi;
-    }
-
-    // Example empty method, return default values
-    public String findPrimes(int input) {
-        return "";
-    }
-
-	@Override
-	public void setInput(String sourceUri) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setOutput(String sinkUri) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setDelimiters(String pairDelimiter, String kvDelimiter) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void useDefaultDelimiters() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public String calculatePrimes(int limit) {
-	    project.checkpoint4.ComputationComponent computation = new project.checkpoint4.ComputationComponent();
-	    List<Integer> primes = computation.compute(limit);
-
-	    if (primes.isEmpty()) {
-	        return "";
+	    public UserAPIImpl(EngineAPI engine, StorageAPI storage) {
+	        this.engine = engine;
+	        this.storage = storage;
 	    }
 
-	    return primes.toString().replace("[", "").replace("]", "").trim();
-	}
+		@Override
+		public void setInput(String sourceUri) {
+			// TODO Auto-generated method stub
+			
+		}
 
+		@Override
+		public void setOutput(String sinkUri) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setDelimiters(String pairDelimiter, String kvDelimiter) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void useDefaultDelimiters() {
+			// TODO Auto-generated method stub
+			
+		}
 }
