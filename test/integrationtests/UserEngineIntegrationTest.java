18b65881b4456dbc8e43b5c0a4505e8a889692da
Ari Adamo <anarianna657@gmail.com>
2025-11-06 10:30:45 -0500

    Update UserEngineIntegrationTest.java

--- test/integrationtests/UserEngineIntegrationTest.java
+++ test/integrationtests/UserEngineIntegrationTest.java
@@ -2,44 +2,31 @@ package integrationtests;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import project.annotations.EngineAPI;
-import project.annotations.StorageAPI;
+import project.EngineAPIImpl;
+import project.StorageAPIImpl;
 import project.UserAPIImpl;
 
 public class UserEngineIntegrationTest {
 
     @Test
-    public void testUserApiAndEngineApiInteraction() {
-        EngineAPI mockEngine = mock(EngineAPI.class);
-        StorageAPI mockStorage = mock(StorageAPI.class);
-        UserAPIImpl userAPI = new UserAPIImpl(mockEngine, mockStorage);
+    public void testUserAndEngineInteraction() {
+        EngineAPIImpl engineApi = new EngineAPIImpl();
+        StorageAPIImpl storageApi = new StorageAPIImpl();
+        UserAPIImpl userApi = new UserAPIImpl(engineApi, storageApi);
 
-        when(mockEngine.calculatePrimes(10)).thenReturn("2, 3, 5, 7");
-
-        String result = userAPI.runEngineTask(mockEngine, 10);
-
-        verify(mockEngine).calculatePrimes(10);
-        assertEquals("2, 3, 5, 7", result);
+        String output = engineApi.calculatePrimes(10);
+        assertNotNull(output);
+        assertEquals("2, 3, 5, 7", output);
     }
 
     @Test
-    public void testUserApiHandlesEngineFailure() {
-        EngineAPI mockEngine = mock(EngineAPI.class);
-        StorageAPI mockStorage = mock(StorageAPI.class);
-        UserAPIImpl userAPI = new UserAPIImpl(mockEngine, mockStorage);
+    public void testEngineComputeIntegration() {
+        EngineAPIImpl engineApi = new EngineAPIImpl();
+        StorageAPIImpl storageApi = new StorageAPIImpl();
+        UserAPIImpl userApi = new UserAPIImpl(engineApi, storageApi);
 
-        when(mockEngine.calculatePrimes(5)).thenThrow(new RuntimeException("Engine crashed"));
-
-        String result;
-        try {
-            result = userAPI.runEngineTask(mockEngine, 5);
-        } catch (Exception e) {
-            result = "error";
-        }
-
+        String result = engineApi.compute(5);
         assertNotNull(result);
     }
 }
+
