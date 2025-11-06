863b5c0a05a2eb11a550c34c70f41f30aef057f4
Ari Adamo <anarianna657@gmail.com>
2025-11-06 12:17:52 -0500

    Update ManualTestingFramework.java

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -10,81 +10,40 @@ public class ManualTestingFramework {
 
     public static void main(String[] args) {
 
-        EngineAPIImpl engine = new EngineAPIImpl() {
-            @Override
-            public String calculatePrimes(int input) {
-                return "2, 3, 5, 7, 11, 13, 17, 19";
-            }
+    	    EngineAPIImpl engine = new EngineAPIImpl() {
+    	        @Override
+    	        public String calculatePrimes(int input) {
+    	            return "2,3,5"; 
+    	        }
+    	    };
 
-			@Override
-			public String compute(int n) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-        };
+    	    StorageAPIImpl storage = new StorageAPIImpl() {
+    	        @Override
+    	        public void saveData(String output, String result) {
+    	            try {
+    	                java.nio.file.Files.write(java.nio.file.Paths.get(output), result.getBytes());
+    	                System.out.println("Output written to " + output);
+    	            } catch (Exception e) {
+    	                e.printStackTrace();
+    	            }
+    	        }
+    	    };
 
-        StorageAPIImpl storage = new StorageAPIImpl() {
-            @Override
-            public void saveData(String output, String result) {
-                System.out.println("Pretending to save to " + output + ": " + result);
-            }
+    	    UserAPIImpl user = new UserAPIImpl(engine, storage) {
+    	        @Override
+    	        public String findPrimes(int input) {
+    	            return engine.calculatePrimes(input);
+    	        }
 
-			@Override
-			public void readInput(String source) {
-				// TODO Auto-generated method stub
-				
-			}
+    	        @Override
+    	        public void displayComputationSummary() {
+    	            System.out.println("Manual test complete.");
+    	        }
+    	    };
 
-			@Override
-			public void writeOutput(String destination) {
-				// TODO Auto-generated method stub
-				
-			}
-        };
-
-        UserAPIImpl user = new UserAPIImpl(engine, storage) {
-            @Override
-            public String findPrimes(int input) {
-                return engine.calculatePrimes(input);
-            }
-
-            @Override
-            public void displayComputationSummary() {
-                System.out.println("Manual test run complete.");
-            }
-
-			@Override
-			public void setInput(String sourceUri) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void setOutput(String sinkUri) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void setDelimiters(String pairDelimiter, String kvDelimiter) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void useDefaultDelimiters() {
-				// TODO Auto-generated method stub
-				
-			}
-        };
-        
-        System.out.println("Running manual test...");
-        String result = user.findPrimes(20);
-        storage.saveData(OUTPUT, result);
-        user.displayComputationSummary();
-
-        System.out.println("Manual test completed successfully!");
-        System.out.println("Input:  " + INPUT);
-        System.out.println("Output: " + OUTPUT);
-    }
+    	    System.out.println("Running manual test...");
+    	    String result = user.findPrimes(3);
+    	    storage.saveData(OUTPUT, result);
+    	    user.displayComputationSummary();
+    	}
 }
