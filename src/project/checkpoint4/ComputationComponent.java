e41aca862fe316037ad10b7d3cbcb99dacdf9150
Ari Adamo <anarianna657@gmail.com>
2025-12-02 20:32:32 -0500

    Improve readability of isPrime method
    Refactor isPrime method for improved readability.

--- src/project/checkpoint4/ComputationComponent.java
+++ src/project/checkpoint4/ComputationComponent.java
@@ -30,9 +30,15 @@ public class ComputationComponent {
 
     private boolean isPrime(int n) {
         try {
-            if (n < 2) return false;
-            if (n == 2) return true;
-            if (n % 2 == 0) return false;
+            if (n < 2) {
+                return false;
+            }
+            if (n == 2) {
+                return true;
+            }
+            if (n % 2 == 0) {
+                return false;
+            }
 
             for (int i = 3; i * i <= n; i += 2) {
                 if (n % i == 0) return false;
