e4003d02eee4dae9eb50f34fce88dc5662bcd959
Ari Adamo <anarianna657@gmail.com>
2025-12-11 18:47:52 -0500

    Update ComputeEngineIntegrationTest.java

--- test/integrationtests/ComputeEngineIntegrationTest.java
+++ test/integrationtests/ComputeEngineIntegrationTest.java
@@ -5,8 +5,8 @@ import project.checkpoint4.ComputationComponent;
 
 import java.nio.file.Path;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ComputeEngineIntegrationTest {
 
