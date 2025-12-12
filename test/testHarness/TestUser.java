597bcec9ebdb7dd133fb2c2a87e64c0286e55d1e
Ari Adamo <anarianna657@gmail.com>
2025-12-11 20:09:13 -0500

    steps a-c
    fixing the todos in testuser and testmultiuser

---
+++ test/testHarness/TestUser.java
@@ -0,0 +1,30 @@
+package testHarness;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import project.checkpoint4.CoordinationComponent;
+
+public class TestUser {
+
+    // TODO 3: change the type of this variable to the name you're using for your
+    // @NetworkAPI interface; also update the parameter passed to the constructor
+    private final CoordinationComponent coordinator;
+
+    public TestUser(CoordinationComponent coordinator) {
+        this.coordinator = coordinator;
+    }
+
+    public void run(String outputPath) {
+        char delimiter = ';';
+        String inputPath = "test" + File.separatorChar + "testInputFile.test";
+
+        // TODO 4: Call the appropriate method(s) on the coordinator to get it to
+        // run the compute job specified by inputPath, outputPath, and delimiter
+        coordinator.startComputation(
+                Path.of(inputPath),
+                Path.of(outputPath)
+        );
+    }
+
+}
